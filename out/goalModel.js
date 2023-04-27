19a3015c21df2adf8d26d79ea7d856cef805a027
Caio Peluti <cpeluti@gmail.com>
2023-04-27 07:48:32 -0300

    mission added to tree view

--- out/goalModel.js
+++ out/goalModel.js
@@ -26,34 +26,39 @@ class GoalModelProvider {
         if (element) {
             // TODO: implement goal recursive getter
             // return Promise.resolve(this.getGoalModelsInGoalModelFolder(path.join(this.workspaceRoot,'gm')));
+            return Promise.resolve(this.getNodesFromMission(element));
             return Promise.resolve([]);
         }
         else {
             const gmFolderPath = path.join(this.workspaceRoot, 'gm');
             if (this.pathExists(gmFolderPath)) {
-                return Promise.resolve(this.getGoalModelsInGoalModelFolder(gmFolderPath));
+                return Promise.resolve(this.getMissionsInGoalModelFolder(gmFolderPath));
             }
             return Promise.resolve([]);
         }
     }
+    getNodesFromMission(mission) {
+        return [];
+    }
     // Should return all the missions inside a GoalModel
-    getGoalModelsInGoalModelFolder(gmFolderPath) {
+    // TODO: parse the goal model in a hierarchical way
+    getMissionsInGoalModelFolder(gmFolderPath) {
         const workspaceRoot = this.workspaceRoot;
         if (this.pathExists(gmFolderPath) && workspaceRoot) {
             const gmList = fs.readdirSync(gmFolderPath);
-            const toMission = (goalModelJson) => {
+            const toMission = (goalModelJson, filePath) => {
                 const actors = goalModelJson.actors;
                 const info = actors.map(actor => {
                     const parsedText = actor.text.split(': ');
-                    return { name: parsedText[1], missionNumber: parsedText[0] };
+                    return { name: parsedText[1], missionNumber: parsedText[0], id: actor.id, nodes: actor.nodes };
                 });
-                return new Mission(info[0].name, info[0].missionNumber, vscode.TreeItemCollapsibleState.Collapsed);
+                return new Mission(info[0].name, info[0].missionNumber, vscode.TreeItemCollapsibleState.Collapsed, filePath, info[0].id, info[0].nodes);
             };
             const gms = gmList.map(gm => {
-                return JSON.parse(fs.readFileSync(path.join(gmFolderPath, gm), 'utf-8'));
+                return { gm: JSON.parse(fs.readFileSync(path.join(gmFolderPath, gm), 'utf-8')), filePath: path.join(gmFolderPath, gm) };
             });
             // console.log(gms);
-            const res = gms.map(gm => toMission(gm));
+            const res = gms.map((gm) => toMission(gm.gm, gm.filePath));
             return res;
         }
         return [];
@@ -71,12 +76,14 @@ class GoalModelProvider {
 }
 exports.GoalModelProvider = GoalModelProvider;
 class Mission extends vscode.TreeItem {
-    constructor(name, missionNumber, collapsibleState, filePath, command) {
+    constructor(name, missionNumber, collapsibleState, filePath, missionId, nodes, command) {
         super(name, collapsibleState);
         this.name = name;
         this.missionNumber = missionNumber;
         this.collapsibleState = collapsibleState;
         this.filePath = filePath;
+        this.missionId = missionId;
+        this.nodes = nodes;
         this.command = command;
         this.contextValue = 'Mission';
         this.tooltip = `${missionNumber}-${this.name}`;
--- src/goalModel.ts
+++ src/goalModel.ts
@@ -1,11 +1,12 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-import { GoalModel } from './GoalModel';
+import { GoalModel, Node } from './GoalModel';
+
 // It's suposed that all goal models are inside the "gm" folder
-export class GoalModelProvider implements vscode.TreeDataProvider<Mission>{
-    private _onDidChangeTreeData: vscode.EventEmitter<Mission | undefined | void> = new vscode.EventEmitter<Mission | undefined | void>();
-    readonly onDidChangeTreeData: vscode.Event<void | Mission | undefined> = this._onDidChangeTreeData.event;
+export class GoalModelProvider implements vscode.TreeDataProvider<Mission | Node>{
+    private _onDidChangeTreeData: vscode.EventEmitter<Mission | Node |undefined | void> = new vscode.EventEmitter<Mission | Node |undefined | void>();
+    readonly onDidChangeTreeData: vscode.Event<void | Mission | Node | undefined> = this._onDidChangeTreeData.event;
     
     constructor(private workspaceRoot: string | undefined){}
 
@@ -18,7 +19,7 @@ export class GoalModelProvider implements vscode.TreeDataProvider<Mission>{
     }
 
     // GetChildren should be triggered by the click on the mission to get the goals;
-    getChildren(element?: Mission | undefined): Thenable<Mission[]> {
+    getChildren(element?: Mission | undefined): Thenable<Mission[] | Node[]> {
         if(!this.workspaceRoot){
             vscode.window.showInformationMessage('No goal models in empty workspace');
             return Promise.resolve([]);
@@ -26,35 +27,40 @@ export class GoalModelProvider implements vscode.TreeDataProvider<Mission>{
         if(element){
             // TODO: implement goal recursive getter
             // return Promise.resolve(this.getGoalModelsInGoalModelFolder(path.join(this.workspaceRoot,'gm')));
+            return Promise.resolve(this.getNodesFromMission(element));
             return Promise.resolve([]);
         } else {
             const gmFolderPath = path.join(this.workspaceRoot, 'gm');
             if(this.pathExists(gmFolderPath)){
-                return Promise.resolve(this.getGoalModelsInGoalModelFolder(gmFolderPath));
+                return Promise.resolve(this.getMissionsInGoalModelFolder(gmFolderPath));
             }   
             return Promise.resolve([]);
         }
     }
+    private getNodesFromMission(mission: Mission): Node[]{
+        return [];
+    }
     // Should return all the missions inside a GoalModel
-    private getGoalModelsInGoalModelFolder(gmFolderPath: string): Mission[]{
+    // TODO: parse the goal model in a hierarchical way
+    private getMissionsInGoalModelFolder(gmFolderPath: string): Mission[]{
         const workspaceRoot = this.workspaceRoot;
         if(this.pathExists(gmFolderPath) && workspaceRoot){
             const gmList = fs.readdirSync(gmFolderPath);
 
-            const toMission = (goalModelJson: GoalModel): Mission => {
+            const toMission = (goalModelJson: GoalModel, filePath: string): Mission => {
                 const actors = goalModelJson.actors;
                 const info = actors.map(actor=>{
                     const parsedText = actor.text.split(': ');
-                    return {name: parsedText[1], missionNumber: parsedText[0]};
+                    return {name: parsedText[1], missionNumber: parsedText[0], id: actor.id, nodes: actor.nodes};
                 });
                 
-                return new Mission(info[0].name, info[0].missionNumber, vscode.TreeItemCollapsibleState.Collapsed, );
+                return new Mission(info[0].name, info[0].missionNumber, vscode.TreeItemCollapsibleState.Collapsed, filePath, info[0].id, info[0].nodes);
             };
-            const gms: GoalModel[] = gmList.map(gm=>{
-                return JSON.parse(fs.readFileSync(path.join(gmFolderPath, gm), 'utf-8'));
+            const gms: {gm:GoalModel,filePath:string}[] = gmList.map(gm=>{
+                return {gm:JSON.parse(fs.readFileSync(path.join(gmFolderPath, gm), 'utf-8')) as GoalModel, filePath: path.join(gmFolderPath, gm)};
             });
             // console.log(gms);
-            const res = gms.map(gm=>toMission(gm));
+            const res = gms.map((gm:{gm:GoalModel, filePath:string})=>toMission(gm.gm, gm.filePath));
             return res;
         }
         return [];
@@ -76,6 +82,8 @@ export class Mission extends vscode.TreeItem {
         private readonly missionNumber: string,
         public readonly collapsibleState: vscode.TreeItemCollapsibleState,
         public readonly filePath: string,
+        public readonly missionId: string,
+        public readonly nodes: Node[],
         public readonly command?: vscode.Command
     ){
         super(name, collapsibleState);
